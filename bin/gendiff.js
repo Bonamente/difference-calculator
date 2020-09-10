f2ab3b9242da3806a6f68555bab9bdea7d42dcae
Bonamente <56506035@protonmail.com>
2020-09-10 11:25:18 +0500

    feat: add arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import program from 'commander';
 
 program
   .description('Compares two configuration files and shows the difference.')
-  .version('1.0.0');
+  .version('1.1.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);
